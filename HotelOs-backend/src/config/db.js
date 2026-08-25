b605ccaa5031ec6d1630f8d0d66a89b83a4e7d24
Subham Duary <subhamduary11@gmail.com>
2026-08-25 19:04:31 +0530

    fix: db dns fix

--- HotelOs-backend/src/config/db.js
+++ HotelOs-backend/src/config/db.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import dns from "node:dns";
 
 const connectDB = async () => {
+  dns.setServers(["8.8.8.8", "1.1.1.1"]);
   try {
     await mongoose.connect(process.env.MONGODB_URI);
 
